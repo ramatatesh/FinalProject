87a02656b790c3a2cfc634abe6615e248ec60edd
قشةشفشفثسا <40@>
2024-11-26 07:53:55 +0530

    FirstCommit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use App\Models\User;
+//use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Auth;
+class UserController extends Controller
+{
+    public function register(Request $request){
+        $request->validate([
+            'username'=>'required|string|max:255',
+            'phone'=>'required|string|min:10',
+            'password'=>'required|string|min:8|confirmed'
+        ]);
+       $user= User::create([
+             'username'=>$request->username,
+            'phone'=>$request->phone,
+            'password'=>Hash::make($request->password)
+        ]);
+         return response()->json([
+           'message'=>'User Registered Successfully',
+           'User'=>$user,
+            201]);
+    }
+
+    public function login(Request $request){
+        $request->validate([
+            'phone'=>'required|string|min:10',
+            'password'=>'required|string|min:8|confirmed'
+        ]);
+
+            if(!Auth::attempt($request->only('phone','password')))
+                return response()->json([
+                'message'=>'invalid phone'],401);
+       $user= User::where('phone',$request->phone)->FirstOrFail();
+       $token= $user->createToken('auth_Token')->plainTextToken;
+          return response()->json([
+            'message'=>'Login Successfully',
+            'User'=>$user,
+            'Token'=>$token]
+            ,201);
+    }
+    public function logout(Request $request){
+     $request->user()->currentAccessToken()->delete();
+     return response()->json([
+        'message'=>'Logout Successful'],201);
+
+    }
+
+}
---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+Route::post('user',[UserController::class,'register']);
+Route::post('login',[UserController::class,'login']);
+Route::post('logout',[UserController::class,'logout'])->middleware('auth:sanctum');
